34412ffca4c3d7e6e67f0625f70d624ec071a93e
Julia Jerg <julia.r.jerg@t-online.de>
2026-05-17 16:30:32 +0200

    erste Struktur und Implementierungen

--- main.c
+++ main.c
@@ -1,14 +1,43 @@
 //vcard
 #include <stdio.h>
+#include <getopt.h>
 
-int main() {
-    char person[256];
-    int attachments = 2;
+int main(int argc, char *argv[]) {
+    int opt;
+    char *vorname = NULL;
+    char *nachname = NULL;
 
-    scanf("%[^\n]", person);
+    while ((opt = getopt(argc, argv, "f:s:")) != -1) {
+        switch (opt) {
+            case 'f':
+                vorname = optarg;
+                break;
+            case 's':
+                nachname = optarg;
+                break;
+        }
+    }
 
-    printf("Hallo %s \n", person);
-    printf("Anhänge: %d \n", attachments);
+    // Hier kommt die Ausgabe
+    
+    if (vorname != NULL && nachname != NULL) {
+        printf("BEGIN:VCARD\n");
+        printf("VERSION:3.0\n");
+        printf("N:%s;%s;;;\n", nachname, vorname);
+        printf("FN:%s %s\n", vorname, nachname);
+        printf("END:VCARD\n");
 
-    return 0;
+        return 0;
+
+    } else {
+        if (vorname == NULL && nachname == NULL) {
+            fprintf(stderr, "Fehler: Kein Vor- und Nachname angegeben!\n");
+        } else if (vorname == NULL) {
+            fprintf(stderr, "Fehler: Kein Vorname angegeben!\n");
+        } else (nachname == NULL) {
+            fprintf(stderr, "Fehler: Kein Nachname angegeben!\n");
+        }
+        return 1;
+    }
+
 }
